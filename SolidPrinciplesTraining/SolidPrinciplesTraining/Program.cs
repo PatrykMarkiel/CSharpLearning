51ed46069143db3a71636919f47121b99df424db
Bułas <patrykmarkiel2137@gmail.com>
2026-08-16 21:41:25 +0200

    SolidPrinciplesTraining project

---
+++ SolidPrinciplesTraining/SolidPrinciplesTraining/Program.cs
@@ -0,0 +1,34 @@
+﻿public interface IWorker
+{
+    void Work();
+}
+public interface IEatable
+{
+    void Eat();
+}
+public interface ISleepable
+{
+    void Sleep();
+}
+public class HumanWorker : IWorker, IEatable, ISleepable
+{
+    public void Work()
+    {
+        Console.WriteLine("Worker is working.");
+    }
+    public void Eat()
+    {
+        Console.WriteLine("Worker is eating.");
+    }
+    public void Sleep()
+    {
+        Console.WriteLine("Worker is sleeping.");
+    }
+}
+public class RobotWorker:IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Robot is working.");
+    }
+}
